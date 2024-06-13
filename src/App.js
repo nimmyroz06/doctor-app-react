c2b8d15d7afc6bf5926539021fee15b2fd3497be
nimmyroz06 <nimmyfisat2023@gmail.com>
2024-06-13 09:46:18 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Adddoctor from './components/Adddoctor';
+import SearchDoc from './components/SearchDoc';
 
 function App() {
   return (
     <div>
       <Adddoctor/>
+      <SearchDoc/>
     </div>
   );
 }
